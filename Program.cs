a2f557ad9fdc77e63fe308765ec5c2eb5a482b95
Akshay Raut <rakshay08@gmail.com>
2024-03-17 01:29:07 +0530

    Taylor Series

--- Program.cs
+++ Program.cs
@@ -13,7 +13,11 @@ namespace AlgoCSharp
             //sedolChecker.Test();
 
             //PowerFunction.PowerBruteForce(2, 9);
-            PowerFunction.PowerOptimized(2, 9);
+            //PowerFunction.PowerOptimized(2, 9);
+
+            Console.WriteLine(TaylorSeries.TaylorMaclaurin(10, 3));
+            var result = TaylorSeries.TaylorMaclaurinMultiReturn(10, 3, 1, 1);
+            Console.WriteLine(result.Item1);
 
             Console.Read();
         }
---
+++ TaylorSeries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoCSharp
+{
+    internal class TaylorSeries
+    {
+        static int p = 1, f = 1;
+        public static long TaylorMaclaurin(int x, int n)
+        {
+            long result = 0;
+            if (n == 0)
+                return 1;
+
+            result = TaylorMaclaurin(x, n - 1);
+            p = p * x;
+            f = f * n;
+            return result + p / f;
+        }
+
+        public static (int, int, int) TaylorMaclaurinMultiReturn(int x, int n, int power, int fact)
+        {
+            int result = 0;
+            if (n == 0)
+                return (1, power, fact);
+
+            (result, power, fact) = TaylorMaclaurinMultiReturn(x, n - 1, power, fact);
+            power = power * x;
+            fact = fact * n;
+            return (result + (power / fact), power, fact);
+        }
+    }
+}
